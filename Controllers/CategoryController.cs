35d9275e36a65fc448ef2cab712ed30ebc9ce3a3
Guilherme Rodrigues Filenga <guilherme.filenga@gmail.com>
2021-03-02 13:27:22 -0300

    Commit inicial

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+
+// https://localhost:5001/categories
+// http://localhost:5000/categories
+[Route("categories")]
+public class CategoryController : ControllerBase
+{
+    [HttpGet]
+    [Route("")]
+    public async Task<ActionResult<List<Category>>> Get()
+    {
+        return new List<Category>();
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<ActionResult<Category>> GetById(int id)
+    {
+        return new Category();
+    }
+
+    [HttpPost]
+    [Route("")]
+    public async Task<ActionResult<List<Category>>> Post([FromBody] Category model)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(model);
+    }
+
+    [HttpPut]
+    [Route("{id:int}")]
+    public async Task<ActionResult<List<Category>>> Put(int id, [FromBody] Category model)
+    {
+        if (model.Id == id)
+            return Ok(model);
+
+        return NotFound();
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    public async Task<ActionResult<List<Category>>> Delete()
+    {
+        return Ok();
+    }
+}
